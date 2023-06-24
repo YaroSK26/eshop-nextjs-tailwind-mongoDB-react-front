92b573528df8dba8753fac3010eb992168d0e4c4
jaxo <jaroba0gmail.com>
2023-06-24 18:19:32 +0200

    front nav spojene s backendom featured product

--- eshop-front/pages/index.js
+++ eshop-front/pages/index.js
@@ -1,11 +1,24 @@
 import Featured from "../components/Featured";
 import Header from "../components/Header";
+import { mongooseConnect } from "../lib/mongoose";
+import {Product} from "../models/Product";
 
-export default function HomePage() {
-  return(
+export default function HomePage({ featuredProduct }) {
+  return (
     <div>
       <Header></Header>
-      <Featured></Featured>
+      <Featured product={featuredProduct}></Featured>
     </div>
-  )
+  );
+}
+
+export async function getServerSideProps() {
+  const featuredProductId = "648f4137afb88f149d29541d";
+  await mongooseConnect();
+  const featuredProduct = await Product.findById(featuredProductId);
+  return {
+    props: {
+      featuredProduct: JSON.parse(JSON.stringify(featuredProduct)),
+    },
+  };
 }
